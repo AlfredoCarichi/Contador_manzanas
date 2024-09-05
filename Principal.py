70e1f0c6a921b182245f8dcb628f50c5032c93b7
AlfredoCarichi <carichibrayan@gmail.com>
2024-09-04 18:15:09 -0600

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -37,7 +37,7 @@ gdown.download(url, output)
 def main():
     
     st.header('Aplicación para la detección de anomalías de NDVI')
-    st.markdown('Esta aplicación esta desarrollada por el Lic. Brayan Murillo y Yulesli Guillén. Para la detección de las anomalías en los valores del NDVI se utiliza una red neuronal convolucional la cual fue entrenada con una GPU RTX 4060, 200 imágenes de entrenamiento y 40 de validación. Se hicieron 100 épocas las cuales dieron como resultado un archivo PyTorch el cual es el que hace las detecciones. Todo fue desarrollado en el lenguaje Python.')
+    st.markdown('Esta aplicación esta desarrollada por el Lic. Brayan Murillo y Yulesli Guillén. Para el conteo de las manzanas en el árbol se utiliza una red neuronal convolucional la cual fue entrenada con una GPU RTX 4060, 1100 imágenes de entrenamiento y 220 de validación. Se hicieron 100 épocas las cuales dieron como resultado un archivo PyTorch el cual es el que hace las detecciones. Todo fue desarrollado en el lenguaje Python.')
     file_uploader = st.file_uploader('Sube tu imagen en los siguientes formatos: ', type=['jpg', 'png'])
 
     if file_uploader is not None:
@@ -47,10 +47,10 @@ def main():
 
         st.image(image)
         datos = deteccion(image)
-        st.markdown('La paleta de colores original fue modificada para poder observar las detecciones de mejor manera. Valores azules representan valores bajos de NDVI')
+        st.markdown('La paleta de colores original fue modificada para poder observar las detecciones de mejor manera.')
         st.image(deteccion((image)))
 
-        st.markdown(deteccion2((image)) + ' anomalías de NDVI detectadas')
+        st.markdown(deteccion2((image)) + ' manzanas detectadas.')
 
         
 
